973d57df097123420a237332d0fa3949e7024a8e
Om Solamki <oms.albiorix@gmail.com>
2023-07-20 14:31:03 +0530

    complete project of food menucard using react-js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 const data = [{
-  name: "tomato pizza",
-  ingredient: "tomatoes,paneer,cheese"
+  name: "Tomato pizza",
+  ingredient: "Tomatoes,paneer,cheese",
+  price: 20,
+  src: "https://images.healthshots.com/healthshots/en/uploads/2023/02/09155200/pizza-1.jpg",
+  soldout: false
 },
 {
-  name: "double cheese",
-  ingredient: "double cheese, capsicum mirchi"
+  name: "Double cheese",
+  ingredient: "Double cheese, capsicum mirchi",
+  price: 20,
+  src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2n6Htee4RyiAPjK2Cp-WWmogBTUE2cw9ghfKMA-CEHrRZ6eEybN3fdFid6iTm8VBz9iY&usqp=CAU",
+  soldout: false
 }, {
-  name: "maxican green wave",
-  ingredient: "mayonese,chiily paneer,mozrella"
+  name: "Maxican green wave",
+  ingredient: "Mayonese,chiily paneer,mozrella",
+  price: 20,
+  src: "https://images.healthshots.com/healthshots/en/uploads/2023/02/09155200/pizza-1.jpg",
+  soldout: true
 }
 ]
 function App() {
@@ -22,46 +31,66 @@ function App() {
     </div>
   );
 }
-function Mydiv(props) {
-  console.log(props)
+function Header() {
+  return <h1 className="text-warning text-center main-title" style={{
+    letterSpacing: "10px"
+  }}> Pizza Hut</h1 >;
+}
+function Menu(props) {
+  const pizzas = data
+  return (
+    <div className="">
+      <div className="menu-title">
+        <h2 className="text-center menu_item">Our Menu</h2>
+        <h3 className="text-center text-success">never sit around and wait for someone unless they're delivering a pizza.</h3>
+      </div>
+      {pizzas && (<ul>
+        {pizzas.map((pizza) => (
+          <Mydiv pizzaobj={pizza} key={pizza.name} />
+        ))}
+      </ul>)}
+    </div>
+  );
+}
+function Mydiv({ pizzaobj }) {
   let hours = new Date().getHours();
   let open = 8;
   let close = 18;
   let isOpen = hours >= open && hours <= close;
   console.log(isOpen);
   return (
-    <div >
+    <div className="main-bodypart">
       <div className="main-image">
         <img
-          className="logoImg"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2n6Htee4RyiAPjK2Cp-WWmogBTUE2cw9ghfKMA-CEHrRZ6eEybN3fdFid6iTm8VBz9iY&usqp=CAU"
+          className={`logoImg ${pizzaobj.soldout ? "sold-out" : ""}`}
+          src={pizzaobj.src}
           alt="logo-img"
         />
       </div>
-      <h4>Double Cheese Pizza</h4>
-      <p>{props.ingredient}</p>
-      <p>{props.name}</p>
+      <h4 className="fw-bold">Double Cheese Pizza</h4>
+      <p className="italic">{pizzaobj.ingredient}</p>
+      <p className="italic">{pizzaobj.name}</p>
+      <p className="italic bold">{pizzaobj.soldout ? "SOLDOUT" : pizzaobj.price} </p>
     </div>
   );
 }
-function Header() {
-  return <h1 className="text-warning">Pizza Hut</h1>;
-}
-function Menu() {
-  return (
-    <div className="">
-      <h3 className="text-center menu_item">Our Menu</h3>
-      <Mydiv name="pizza" ingredient="chilly,cheese,toamto" />
-    </div>
-  );
-}
-function Footer() {
+function Footer(props) {
+  let hours = new Date().getHours();
+  let open = 11;
+  let close = 18;
+  let isOpen = hours >= open && hours <= close;
+
   return (
     <footer>
-      <h3 className="text-center text-danger">
-        {" "}
-        {new Date().toLocaleTimeString()}We're Currently Open{" "}
-      </h3>
+      {isOpen && (
+        <div className="main-bodypart">
+          <p>
+            We're open until {close}:00 come visit for order online.
+          </p>
+          <button className="btn btn-warning ">Order</button>
+        </div>
+      )
+      }
     </footer>
   );
 }
