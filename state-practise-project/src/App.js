63bf41a792ff7735d20bc30277a0b07f304425be
Om Solamki <oms.albiorix@gmail.com>
2023-07-20 20:06:55 +0530

    demo project

--- state-practise-project/src/App.js
+++ state-practise-project/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = ["Learn React  ", "Apply for Job", "Invest your Job"];
 
 function App() {
+  const [step, setStep] = useState(1);
+  // const [test, setTest] = useState({ name: "om" })
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1)
+  }
+
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1)
+    // setTest({ name: "yash" })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button className="close btn btn-primary  m-4 p-2 border" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+        <div className="App" >
+          <div className="step">
+            <div className="numbers">
+              <div className={`indivisualnumber ${step >= 1 ? "active" : ""}`}>
+                1
+              </div>
+              <div className={`indivisualnumber ${step >= 2 ? "active" : ""}`}>
+                2
+              </div>
+              <div className={`indivisualnumber ${step >= 3 ? "active" : ""}`}>
+                3
+              </div>
+            </div>
+            <p className="text-center">
+              Step {step}: {messages[step - 1]}
+              {/* {test.name} */}
+
+            </p>
+            <div className="button merged">
+              <button
+                className="btn btn-secondary buttonprevious"
+                onClick={() => handlePrevious()}
+              >
+                Previous
+              </button>
+              <button className="btn btn-secondary buttonnext" onClick={() => handleNext()}>
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+      }</>)
+
+
 }
 
 export default App;
