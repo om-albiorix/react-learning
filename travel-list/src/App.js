0ae1d90fb096b4f99bfabb5a9033ff8037993f51
Om Solamki <oms.albiorix@gmail.com>
2023-07-21 12:49:13 +0530

    travel list project setup

---
+++ travel-list/src/App.js
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <h1>Far Away</h1>
+    </div>
+  )
+}
+
+function form() {
+}
+function PackingLists() {
+}
+function States() {
+}
+
+export default App;
