81ffde09a3fb4a4d1cbd4c7644cdb00c0e659da5
Om Solamki <oms.albiorix@gmail.com>
2023-07-25 19:37:40 +0530

    create new project of use popcorn

---
+++ usepopcorn/src/App.js
@@ -0,0 +1,227 @@
+import { useState } from "react";
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+
+export default function App() {
+
+  return (
+    <div className="main-div">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+const Navbar = () => {
+  return <div>
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults />
+    </nav>
+  </div>
+}
+const Logo = () => {
+  return (<div className="logo">
+    <span role="img">🍿</span>
+    <h1>usePopcorn</h1>
+  </div>)
+}
+const Search = () => {
+  const [query, setQuery] = useState("");
+
+  return (<input
+    className="search"
+    type="text"
+    placeholder="Search movies..."
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}
+  />)
+}
+
+const NumResults = () => {
+  return (
+    <p className="num-results">
+      Found <strong>X results</strong> results
+    </p>)
+}
+
+const Main = () => {
+  return (<main className="main">
+    <Listbox />
+    <WatchBox />
+  </main>)
+}
+const WatchMovieList = ({ watched }) => {
+
+  return <div>
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} />
+      ))}
+    </ul>
+  </div>
+}
+const WatchedMovie = ({ movie }) => {
+  return <div>
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
+  </div>
+}
+
+const Listbox = () => {
+  const [isOpen1, setIsOpen1] = useState(true);
+  return (<div className="box">
+    <button
+      className="btn-toggle"
+      onClick={() => setIsOpen1((open) => !open)}
+    >
+      {isOpen1 ? "–" : "+"}
+    </button>
+    {isOpen1 && (
+      <MovieList />
+    )}
+  </div>)
+}
+
+const MovieList = () => {
+  const [movies, setMovies] = useState(tempMovieData);
+
+  return (
+    <ul className="list" >
+      {movies?.map((movie) => (
+        <Movie movie={movie} />
+      ))
+      }
+    </ul>
+  )
+}
+
+const Movie = ({ movie }) => {
+  return (<li key={
+    movie.imdbID
+  } >
+    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+    <h3>{movie.Title}</h3>
+    <div>
+      <p>
+        <span>🗓</span>
+        <span>{movie.Year}</span>
+      </p>
+    </div>
+  </li>)
+}
+
+const WatchBox = () => {
+  const [isOpen2, setIsOpen2] = useState(true);
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <Summary />
+      )}
+    </div>
+  )
+}
+
+
+const Summary = () => {
+  const [watched, setWatched] = useState(tempWatchedData);
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+      <WatchMovieList watched={watched} />
+    </div>
+  )
+}
