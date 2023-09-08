a0966ba73381dd9227e1681c5e4c96a38cadc4dd
Om Solamki <oms.albiorix@gmail.com>
2023-09-08 17:42:45 +0530

    create demo project and implemeting functionality of add to cart and order add ,delete update and tailwind css

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
+
 const Header = () => {
   return (
-    <div>
-      <Link to="/">Fast React Pizza</Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+
       <SearchOrder />
-    </div>
+      <Username />
+    </header>
   );
 };
+
 export default Header;
